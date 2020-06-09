a724722ade1426ea1a23e8642003a883c21290c6
Majid Alturki <majidalturki@live.com>
2020-06-08 18:24:34 -0700

    rotate, UI

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public CharacterController controller;
+    public float speed;
+
+    public float turnSpeed = 50;
+    
+    private float score = 0;
+
+    public Text scoreText;
     void Awake()
     {
         Debug.Log("Awake was called");
@@ -17,10 +26,33 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.Translate(transform.forward);
-        }
+        float verticalInput = Input.GetAxis("Vertical");
+        controller.SimpleMove(transform.forward * speed * verticalInput * Time.deltaTime);
 
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 rotation = new Vector3(0, horizontalInput * turnSpeed * Time.deltaTime, 0);
+        transform.Rotate(rotation);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Collectible")){
+            Collectibles collectible = other.gameObject.GetComponent<Collectibles>();
+            score = score + collectible.point;
+            Destroy(other.gameObject);
+
+            scoreText.text = "Score: " + score.ToString();
+
+            Debug.Log("Current score: " + score);
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        Debug.Log("Trigger Exit");
+    }
+
+    void OnTriggerStay(Collider other){
+        Debug.Log("Trigger Stay");
     }
 }
